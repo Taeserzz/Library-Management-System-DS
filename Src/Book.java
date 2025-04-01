f923f6a144afd9e613fc50d146057b3fc39000b9
La-Tavia <pearcelatavia@gmail.com>
2025-04-01 07:28:01 -0500

    Update Book.java

--- Src/Book.java
+++ Src/Book.java
@@ -1,8 +1,10 @@
 // Class representing a book in the library
-class Book {
-    String title, author, ISBN;
-    boolean available;
-    Book next;
+public class Book {
+    private String title; 
+    private String author;
+    private String ISBN;
+    private boolean available;
+    public Book next;
 
     // Constructor to initialize a book
     public Book(String title, String author, String ISBN) {
@@ -12,4 +14,13 @@ class Book {
         this.available = true; // Book is available by default
         this.next = null;
     }
+
+    // Getters and setters 
+    public String getTitle() { return title; }
+    public String getAuthur() { return author; }
+    public String getISBN() { return ISBN; }
+    public boolean isAvailable() { return available; } 
+
+
+    public void setAvailable(boolean status) {this.available = status; }
 }
