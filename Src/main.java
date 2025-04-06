a3c19d0b06a7a6865127f26f6d52bd81e56e9166
La-Tavia <pearcelatavia@gmail.com>
2025-04-06 07:30:00 -0500

    Update main.java

--- Src/main.java
+++ Src/main.java
@@ -1,5 +1,6 @@
 public class main {
   public static void main(String[] args) {
+    System.out.println("Launching Library Management System...");
     // Launch the main user interface 
     UI.main(args);
   }
